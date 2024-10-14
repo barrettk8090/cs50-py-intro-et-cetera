c5f28603270b8a94363d1ec6d3b81c8ef7682c23
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-14 12:22:08 -0600

    type hints

---
+++ type-hints.py
@@ -0,0 +1,7 @@
+# hint that n should be an int, and this method should return -> a str
+def meow(n: int) -> str:
+    return "meow\n" * n
+
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows, end="")
