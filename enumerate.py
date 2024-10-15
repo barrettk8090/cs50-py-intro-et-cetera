6518ef847994376283e0ebccdafddd9a09dfd763
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-15 16:05:47 -0600

    enumerate

---
+++ enumerate.py
@@ -0,0 +1,13 @@
+students = ["Hermione", "Harry", "Ron"]
+
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+# 1 Hermione 
+# 2 Harry
+# 3 Ron 
+
+# Using enumerate: 
+
+for i, student in enumerate(students):
+    print(i + 1, student)
