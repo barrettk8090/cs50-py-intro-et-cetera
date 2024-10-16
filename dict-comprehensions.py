7662df3bc9146289650a8e2ff1d1566b7b8a2165
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-16 08:11:30 -0600

    complete

--- dict-comprehensions.py
+++ dict-comprehensions.py
@@ -5,10 +5,10 @@ students = ["Hermione", "Harry", "Ron"]
 # for student in students:
 #     gryffindors.append({"name": student, "house": "Gryffindor"})
 
-# Using dictionary comprehension - list of dictionaries
+# Using list & dictionary comprehension - list of dictionaries
 gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
 
-# Even more simplified - just a single dictionary
+# Even more simplified, just using dictionary comprehensions  - just a single dictionary
 gryffindors2 = {student: "Gryffindor" for student in students}
 
 print(gryffindors2)
